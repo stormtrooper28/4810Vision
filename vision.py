cce21fb7ed70435a6e2bc394b5c4ea7042b9c5c7
Alex Phan <alexp7361@gmail.com>
2016-04-09 12:53:38 -0400

    Added some comments to the code
    Signed-off-by: Alex Phan <alexp7361@gmail.com>

--- vision.py
+++ vision.py
@@ -3,42 +3,49 @@ import cv2
 import math
 import time
 
+#Returns the coverage area ratio, defined by the ratio of the area of the particle to the area of the bounding box drawn around  the circle
 def coverage_area_ratio(par_area, box_area):
 	return par_area/box_area
 	
+#Returns the aspect ratio, defined  by the ratio of the height of the bounding box to the width of the bounding box	
 def aspect_ratio(width, height):
 	return width/height
 	
+#Returns a score, defined by 100 - (percent error * 100)
 def get_score(score, ideal):
 	diff = abs(score - ideal)
 	error_pct = ((diff / ideal) * 100)
 	return math.floor(100 - error_pct)
 	
+#Returns a boolean determining a target, a predefined goal is set
 def determine_target(cov_score, asp_score):
 	if cov_score >= 70 & asp_score >= 70:
 		return True
 	else:
 		return False
 	
+#The main vision algorithm
 def vision():
+	#capture a video
 	cap = cv2.VideoCapture(0)
 	cov_sc = 0
 	asp_sc = 0
 	
 	while True:
-	
+		#Do the processing
 		#orig = cv2.imread('samplepics/pic2.jpg', cv2.IMREAD_COLOR)
 		ret, orig = cap.read()
 		hsv = cv2.cvtColor(orig, cv2.COLOR_BGR2HSV)
 		
-		
+		#numpy arrays to store bgr values to mask
 		lower_red = np.array([50, 0, 225], dtype = np.uint8) 
 		upper_red = np.array([70, 10, 255], dtype = np.uint8)
 		
+		#masking and thresholding the image
 		mask = cv2.inRange(hsv, lower_red, upper_red)
 		ret, thresh = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
 		
-		
+		#find the contours of the image to find a boundary
 		_, contours, hierarchy  = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 		areas = [cv2.contourArea(c) for c in contours]
 		if not areas:
